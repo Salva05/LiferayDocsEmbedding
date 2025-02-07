3794577de045d121a54dbe8ec4192eaf19905313
Salva05 <salvatorelombardi.98@hotmail.it>
2025-02-07 14:20:36 +0100

    Created metadata_extractor function in utils.py to get metadata from JSONL record.

---
+++ utils.py
@@ -0,0 +1,27 @@
+def metadata_extractor(record: dict, metadata: dict) -> dict:
+    """
+    Extracts each key that is not 'content' from a DocumentationItem JSON record
+    and returns it as the metadata of a LangChain Document.
+    (for more information see https://github.com/Salva05/LiferayDocsScrape.git)
+
+    Args:
+        record (dict): The input dictionary containing the scraped document data.
+        metadata (dict): A dictionary where extracted metadata will be stored.
+
+    Returns:
+        dict: A dictionary containing the structured metadata for the document.
+    """
+
+    # Extract common metadata fields from the scraped record
+    metadata["url"] = record.get("url")
+    metadata["title"] = record.get("title")
+    metadata["path"] = record.get("path")
+    metadata["scraped_at"] = record.get("scraped_at")
+
+    # Extract metadata field, but excludes the field Deployment Approach (not really relevant)
+    metadata["metadata"] = {
+        key: value
+        for key, value in filter(lambda item: item[0] != "Deployment Approach", record.get("metadata", {}).items())
+    }
+
+    return metadata
